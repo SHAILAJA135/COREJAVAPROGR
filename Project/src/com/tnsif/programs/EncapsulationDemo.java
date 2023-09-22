9a63457ab230bacd68c5a25235aabbcac8e8d1ea
acer <acer@10.0.0.123>
2023-09-22 15:38:55 +0530

    First commit

---
+++ Project/src/com/tnsif/programs/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.programs;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Encapsulation ob=new Encapsulation();
+      ob.serialno=33;
+      ob.name="sravya";
+      ob.age=21;
+      ob.show();
+	}
+
+}
