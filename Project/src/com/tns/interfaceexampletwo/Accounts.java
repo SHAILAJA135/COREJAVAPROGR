d1dece980790f3937131cf04c3bad69969aafe82
acer <acer@10.0.0.136>
2023-10-05 16:47:31 +0530

    First commit

---
+++ Project/src/com/tns/interfaceexampletwo/Accounts.java
@@ -0,0 +1,6 @@
+package com.tns.interfaceexampletwo;
+
+public interface Accounts {
+           void deliveryCharges();//abstract methods
+         //  int a;//can't declare instance veriables
+}
---
+++ Project/src/com/tns/interfaceexampletwo/NonPrime.java
@@ -0,0 +1,12 @@
+package com.tns.interfaceexampletwo;
+
+public class NonPrime implements Accounts {
+
+	@Override
+	public void deliveryCharges() {
+		// TODO Auto-generated method stub
+		int amount=100;
+		System.out.println("The delivery charges for NonPrime account is"+100);
+	}
+
+}
