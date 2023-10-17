79a9eba27d4c734144a9832de53f117d5ccafb94
acer <acer@10.0.0.106>
2023-10-17 14:04:21 +0530

    First commit

---
+++ Project/src/com/tns/exception/Age.java
@@ -0,0 +1,7 @@
+package com.tns.exception;
+
+public class Age extends Exception {//Custom class extending Exception
+Age(String str){
+	System.out.println(str);//create a constructor with parameter to provide
+}
+}
